c1721aa6640c6dfc9976c7aa6480edbc6925b376
masan4444 <masanori0213@gmail.com>
2021-05-04 00:14:39 +0900

    add and sub

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,39 @@
-use std::env;
-use std::error::Error;
+use std::{env, error::Error, iter::Peekable};
 
 fn main() -> Result<(), Box<dyn Error>> {
     let args: Vec<_> = env::args().collect();
-    if args.len() != 2 {
-        eprintln!("invalid argument count");
-        std::process::exit(1);
-    }
-    let arg: u8 = args[1].parse()?;
+    let mut chars = args
+        .get(1)
+        .ok_or("invalid number of arguments")?
+        .chars()
+        .peekable();
+
     println!(".intel_syntax noprefix");
     println!(".globl main");
     println!("main:");
-    println!("  mov rax, {}", arg);
+    println!("  mov rax, {}", strtol(&mut chars, 10).unwrap());
+
+    while let Some(n) = chars.next() {
+        match n {
+            '+' => println!("  add rax, {}", strtol(&mut chars, 10).unwrap()),
+            '-' => println!("  sub rax, {}", strtol(&mut chars, 10).unwrap()),
+            _ => return Err("invalid char")?,
+        }
+    }
     println!("  ret");
     Ok(())
+}
+
+fn strtol<I: Iterator<Item = char>>(iter: &mut Peekable<I>, radix: u32) -> Option<u32> {
+    let mut init = iter.peek()?.to_digit(radix)?;
+    iter.next();
+    while let Some(c) = iter.peek() {
+        if let Some(n) = c.to_digit(radix) {
+            init = init * radix + n;
+            iter.next();
+        } else {
+            break;
+        }
+    }
+    Some(init)
 }
